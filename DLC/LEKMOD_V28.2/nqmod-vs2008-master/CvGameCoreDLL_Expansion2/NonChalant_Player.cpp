0a0110d7c3cdd95e83f2d182aebc56ce3d3aa83b
Chalant <Strikerklm96@gmail.com>
2022-10-31 22:53:18 -0500

    add city sources

--- DLC/LEKMOD_V28.2/nqmod-vs2008-master/CvGameCoreDLL_Expansion2/NonChalant_Player.cpp
+++ DLC/LEKMOD_V28.2/nqmod-vs2008-master/CvGameCoreDLL_Expansion2/NonChalant_Player.cpp
@@ -40,32 +40,48 @@ string CvPlayer::GetCityCapCurrent_WithSourcesTooltip(int* sum) const
 		appendNewLine(&ss, sum, "Base", baseNum, true);
 	}
 
-	// 1 for every other era, first unlock in classical
-	{
-		// ancient era is id 0, classical is id 1...
-		const int Classical = 1;
-		appendNewLine(&ss, sum, "from the {TXT_KEY_ERA_1}", Classical, player.GetCurrentEra() >= 1); // {} evaluates to Classical Era
-
-		const int Renaissance = 1;
-		appendNewLine(&ss, sum, "from the {TXT_KEY_ERA_3}", Renaissance, player.GetCurrentEra() >= 3);
-
-		const int Modern = 1;
-		appendNewLine(&ss, sum, "from the {TXT_KEY_ERA_5}", Modern, player.GetCurrentEra() >= 5);
-
-		const int Future = 1;
-		appendNewLine(&ss, sum, "from the {TXT_KEY_ERA_7}", Future, player.GetCurrentEra() >= 7);
-		// last era is 8 (future)
-	}
-
-	{ // 1 per courthouse
-		const int numCourthouses = player.countNumBuildingClasses(BuildingClass("BUILDINGCLASS_COURTHOUSE"));
-		appendNewLine(&ss, sum, "from 1 per Courthouse", numCourthouses, numCourthouses > 0);
+	{ // liberty finisher
+		appendNewLine(&ss, sum, "from completing the Liberty Policy Tree", 1, player.HasPolicy("POLICY_LIBERTY_FINISHER"));
 	}
 
 	{ // 1 for 8 policies (free policies included)
 		const int numPolicies = player.GetPlayerPolicies()->GetNumPoliciesOwned();
 		appendNewLine(&ss, sum, "from 8 or more Social Policies", 1, player.GetNumPolicies() >= 8);
 	}
+	
+	{ // 1 for every other era, first unlock in classical
+		// ancient era is id 0, classical is id 1...
+		appendNewLine(&ss, sum, "from the {TXT_KEY_ERA_1}", 1, player.GetCurrentEra() >= 1); // {} evaluates to Classical Era
+
+		appendNewLine(&ss, sum, "from the {TXT_KEY_ERA_3}", 1, player.GetCurrentEra() >= 3);
+
+		appendNewLine(&ss, sum, "from the {TXT_KEY_ERA_5}", 1, player.GetCurrentEra() >= 5);
+
+		appendNewLine(&ss, sum, "from the {TXT_KEY_ERA_7}", 1, player.GetCurrentEra() >= 7);
+		// last era is 8 (future)
+	}
+
+	{ // 1 per courthouse
+		const int numCourthouses = player.countNumBuildingClasses(BuildingClass("BUILDINGCLASS_COURTHOUSE"));
+		int maxCourthouseBonus = 2;
+		// iron curtain +1
+		if (player.HasPolicy("POLICY_NEW_ORDER"))
+			maxCourthouseBonus += 2;
+		if (player.HasPolicy("POLICY_HONOR_FINISHER"))
+			maxCourthouseBonus += 1;
+
+		stringstream desc;
+		desc << "from 1 per Courthouse (max " << maxCourthouseBonus << ")";
+		appendNewLine(&ss, sum, desc.str(), min(maxCourthouseBonus, numCourthouses), numCourthouses > 0);
+	}
+
+	{ // iron curtain +2
+		appendNewLine(&ss, sum, "from the Iron Curtain Tenet", 2, player.HasPolicy("POLICY_IRON_CURTAIN"));
+	}
+
+	//{ // wonder
+	//	appendNewLine(&ss, sum, "from ", 1, player.countNumBuildingClasses(BuildingClass("")));
+	//}
 
 	// ai get to build as many as they want
 	if (!isHuman())
