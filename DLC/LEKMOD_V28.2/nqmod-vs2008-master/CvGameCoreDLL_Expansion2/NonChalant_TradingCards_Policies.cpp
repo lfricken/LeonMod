45a17b2613b839279369bd97ff167156f35fe99b
Chalant <Strikerklm96@gmail.com>
2023-01-21 11:24:46 -0600

    active example

--- DLC/LEKMOD_V28.2/nqmod-vs2008-master/CvGameCoreDLL_Expansion2/NonChalant_TradingCards_Policies.cpp
+++ DLC/LEKMOD_V28.2/nqmod-vs2008-master/CvGameCoreDLL_Expansion2/NonChalant_TradingCards_Policies.cpp
@@ -8,6 +8,7 @@ string TradingCard::GetActivePolicy(TradingCardTypes type)
 {
 	switch (type)
 	{
+	case CARD_NAVAL_MOVES: return "CARD_NAVAL_MOVES_ACTIVE";
 	case CARD_RANDOM_SPY: return "CARD_RANDOM_SPY";
 	default: return "";
 	};
