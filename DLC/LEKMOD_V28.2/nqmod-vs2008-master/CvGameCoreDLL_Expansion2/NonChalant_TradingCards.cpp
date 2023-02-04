ae0e7069e01a50c8b2df9977e6c2fc4e97814adc
Chalant <Strikerklm96@gmail.com>
2023-02-03 20:31:14 -0600

    card name random space fix

--- DLC/LEKMOD_V28.2/nqmod-vs2008-master/CvGameCoreDLL_Expansion2/NonChalant_TradingCards.cpp
+++ DLC/LEKMOD_V28.2/nqmod-vs2008-master/CvGameCoreDLL_Expansion2/NonChalant_TradingCards.cpp
@@ -91,7 +91,7 @@ string TradingCard::GetName(TradingCardTypes type, CvPlayer* player)
 	const CvPolicyEntry* passiveInfo = GetPolicyInfo(TradingCard::GetPassivePolicy(type));
 	string passive = passiveInfo == NULL ? "" : passiveInfo->GetDescription();
 	string joiner = "";
-	if (active.size() != 0 && active.size() != 0)
+	if (passive.size() != 0 && active.size() != 0)
 	{
 		joiner = " ";
 	}
@@ -104,7 +104,7 @@ string TradingCard::GetDesc(TradingCardTypes type, CvPlayer* player)
 	const CvPolicyEntry* passiveInfo = GetPolicyInfo(TradingCard::GetPassivePolicy(type));
 	string passive = passiveInfo == NULL ? "" : passiveInfo->GetHelp();
 	string joiner = "";
-	if (active.size() != 0 && active.size() != 0)
+	if (passive.size() != 0 && active.size() != 0)
 	{
 		joiner = " ";
 	}
