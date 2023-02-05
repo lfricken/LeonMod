0a3f7b68f20bee5d3e53824157f795f1088b2bbf
Chalant <Strikerklm96@gmail.com>
2023-02-05 16:15:58 -0600

    reverse card condition

--- DLC/LEKMOD_V28.2/nqmod-vs2008-master/CvGameCoreDLL_Expansion2/NonChalant_TradingCards_Policies.cpp
+++ DLC/LEKMOD_V28.2/nqmod-vs2008-master/CvGameCoreDLL_Expansion2/NonChalant_TradingCards_Policies.cpp
@@ -51,105 +51,104 @@ bool TradingCard::IsConditionSatisfied(TradingCardTypes type, const CvPlayer* pP
 	int futureEra = 9;
 	switch (type)
 	{
-
-	case 142: // POLICY_CARD_ANCIENT_RESOURCES_ATLATL_PASSIVE
-	{
-		int numDeerPlotsOwned = player.CountOwnedPlots(DeerPlots);
-		return (numDeerPlotsOwned < 6);
-	}
 	case 128: // POLICY_CARD_ANCIENT_UNITS_FAVORABLE_WINDS_PASSIVE
 	{
-		return (playerEra >= medievalEra);
+		return !(playerEra >= medievalEra);
 	}
 	case 129: // POLICY_CARD_ANCIENT_UNITS_SACRIFICIAL_CAPTIVES_PASSIVE
 	{
-		return (playerEra >= medievalEra);
+		return !(playerEra >= medievalEra);
 	}
 	case 139: // POLICY_CARD_ANCIENT_UNITS_VALOUR_PASSIVE
 	{
-		return (playerEra >= medievalEra);
+		return !(playerEra >= medievalEra);
 	}
 	case 140: // POLICY_CARD_ANCIENT_RESOURCES_INUITS_PASSIVE
 	{
 		int numTundraPlotsOwned = player.CountOwnedPlots(TundraTiles);
-		return (numTundraPlotsOwned < 10);
+		return !(numTundraPlotsOwned < 10);
 	}
 	case 141: // POLICY_CARD_ANCIENT_RESOURCES_BEDOUINS_PASSIVE
 	{
 		int numDesertPlotsOwned = player.CountOwnedPlots(DesertTiles);
-		return (numDesertPlotsOwned < 10);
+		return !(numDesertPlotsOwned < 10);
+	}
+	case 142: // POLICY_CARD_ANCIENT_RESOURCES_ATLATL_PASSIVE
+	{
+		int numDeerPlotsOwned = player.CountOwnedPlots(DeerPlots);
+		return !(numDeerPlotsOwned < 6);
 	}
 	case 145: // POLICY_CARD_ANCIENT_RESOURCES_SPEAR_FISHING_PASSIVE
 	{
-		return (playerEra > classicalEra);
+		return !(playerEra > classicalEra);
 	}
 	case 149: // POLICY_CARD_ANCIENT_POLITICAL_ORTHODOXY_PASSIVE
 	{
 		int numTraditionPolicies = player.GetPlayerPolicies()->GetNumPoliciesOwnedInBranch(PolicyBranchTypes(0));
-		return (numTraditionPolicies < 3);
+		return !(numTraditionPolicies < 3);
 	}
 	case 150: // POLICY_CARD_ANCIENT_POLITICAL_PROGRESSIVE_PASSIVE
 	{
 		int numLibertyPolicies = player.GetPlayerPolicies()->GetNumPoliciesOwnedInBranch(PolicyBranchTypes(1));
-		return (numLibertyPolicies < 3);
+		return !(numLibertyPolicies < 3);
 	}
 	case 151: // POLICY_CARD_ANCIENT_POLITICAL_AGGRESIVE_PASSIVE
 	{
 		int numHonorPolicies = player.GetPlayerPolicies()->GetNumPoliciesOwnedInBranch(PolicyBranchTypes(2));
-		return (numHonorPolicies < 3);
+		return !(numHonorPolicies < 3);
 	}
 	case 152: // POLICY_CARD_ANCIENT_POLITICAL_EXCLUSIVE_PASSIVE
 	{
 		int numLibertyPolicies = player.GetPlayerPolicies()->GetNumPoliciesOwnedInBranch(PolicyBranchTypes(1));
 		int numPolicies = player.GetNumPoliciesTotal();
 		const bool satisfied = ((numLibertyPolicies == 0) && (numPolicies >= 5));
-		return (!satisfied);
+		return !(!satisfied);
 	}
 	case 155: // POLICY_CARD_ANCIENT_POLITICAL_PROTECTIVE_PASSIVE
 	{
 		int numHonorPolicies = player.GetPlayerPolicies()->GetNumPoliciesOwnedInBranch(PolicyBranchTypes(2));
 		int numPolicies = player.GetNumPoliciesTotal();
 		const bool satisfied = ((numHonorPolicies == 0) && (numPolicies >= 5));
-		return (!satisfied);
+		return !(!satisfied);
 	}
 	case 156: // POLICY_CARD_CLASSICAL_RESOURCE_LIMESTONE_PASSIVE
 	{
 		int numStonePlotsOwned = player.CountOwnedPlots(StonePlots);
-		return (numStonePlotsOwned < 3);
+		return !(numStonePlotsOwned < 3);
 	}
 	case 157: // POLICY_CARD_CLASSICAL_UNITS_INDUSTRIOUS_PASSIVE
 	{
-		return (playerEra >= industrialEra);
+		return !(playerEra >= industrialEra);
 	}
 	case 158: // POLICY_CARD_CLASSICAL_UNITS_FORCED_MARCH_PASSIVE
 	{
-		return (playerEra >= industrialEra);
+		return !(playerEra >= industrialEra);
 	}
 	case 159: // POLICY_CARD_CLASSICAL_UNITS_SIEGE_ENGINEERS_PASSIVE
 	{
-		return (playerEra >= industrialEra);
+		return !(playerEra >= industrialEra);
 	}
 	case 168: // POLICY_CARD_MEDIEVAL_RESOURCE_GILLNETS_PASSIVE
 	{
-		return (playerEra >= industrialEra);
+		return !(playerEra >= industrialEra);
 	}
 	case 175: // POLICY_CARD_MEDIEVAL_UNITS_LANTEEN_SAILS_PASSIVE
 	{
-		return (playerEra >= industrialEra);
+		return !(playerEra >= industrialEra);
 	}
 	case 184: // POLICY_CARD_ANCIENT_WONDERS_HIPPODAMUS_OF_MILETUS_PASSIVE
 	{
-		return (playerEra >= medievalEra);
+		return !(playerEra >= medievalEra);
 	}
 	case 186: // POLICY_CARD_CLASSICAL_BUILDINGS_CANNON_OF_TEN_PASSIVE
 	{
 		int numAmphitheaters = player.countNumBuildingClasses(BuildingClassTypes(19));
-		return (numAmphitheaters < 10);
+		return !(numAmphitheaters < 10);
 	}
 	case 188: // POLICY_CARD_MEDIEVAL_BUILDINGS_FEALTY_ACTIVE
 	{
 		int numCastles = player.countNumBuildingClasses(BuildingClassTypes(19));
-		return (numCastles < 6);
+		return !(numCastles < 6);
 	}
 
 
