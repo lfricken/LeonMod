b67f1205664296561d394f60da8545a85068c0bd
Chalant <Strikerklm96@gmail.com>
2023-02-16 21:18:26 -0600

    growth algorithms

--- Maps/ANC_LandAndSea.lua
+++ Maps/ANC_LandAndSea.lua
@@ -4,15 +4,73 @@
 include("ANC_Utils");
 
 
+function fill(bWouldConnect,numAdjacent)
+	if (bWouldConnect) then return false; end
+	local doMutate = false;
+
+	if (numAdjacent == 1) then return false;
+	elseif (numAdjacent == 2) then return (Map.Rand(1000, "Mutate") < 100);
+	elseif (numAdjacent == 3) then return (Map.Rand(1000, "Mutate") < 300);
+	elseif (numAdjacent == 4) then return (Map.Rand(1000, "Mutate") < 500);
+	elseif (numAdjacent >= 5) then return (Map.Rand(1000, "Mutate") < 800); end
+	return false;
+end
+function tendril(bWouldConnect,numAdjacent)
+	if (bWouldConnect) then return false; end
+	local doMutate = false;
+
+	if (numAdjacent == 1) then return (Map.Rand(1000, "Mutate") < 800);
+	elseif (numAdjacent == 2) then return (Map.Rand(1000, "Mutate") < 500);
+	elseif (numAdjacent == 3) then return (Map.Rand(1000, "Mutate") < 300);
+	elseif (numAdjacent == 4) then return (Map.Rand(1000, "Mutate") < 100);
+	elseif (numAdjacent >= 5) then return false; end
+	return false;
+end
+function expand(bWouldConnect,numAdjacent)
+	if (bWouldConnect) then return false; end
+	local doMutate = false;
+
+	if (numAdjacent == 1) then return (Map.Rand(1000, "Mutate") < 100);
+	elseif (numAdjacent == 2) then return (Map.Rand(1000, "Mutate") < 200);
+	elseif (numAdjacent == 3) then return (Map.Rand(1000, "Mutate") < 300);
+	elseif (numAdjacent == 4) then return (Map.Rand(1000, "Mutate") < 400);
+	elseif (numAdjacent >= 5) then return (Map.Rand(1000, "Mutate") < 500); end
+	return false;
+end
+
+
+function growLand(this)
+	local from, to = PlotTypes.PLOT_OCEAN, PlotTypes.PLOT_LAND;
+	Mutate(this, from, to, nil, tendril);
+end
+function fillLand(this)
+	local from, to = PlotTypes.PLOT_OCEAN, PlotTypes.PLOT_LAND;
+	Mutate(this, from, to, nil, fill);
+end
+function growWater(this)
+	local from, to = PlotTypes.PLOT_LAND, PlotTypes.PLOT_OCEAN;
+	Mutate(this, from, to, nil, tendril);
+end
+function fillWater(this)
+	local from, to = PlotTypes.PLOT_LAND, PlotTypes.PLOT_OCEAN;
+	Mutate(this, from, to, nil, fill);
+end
+function expandWater(this)
+	local from, to = PlotTypes.PLOT_LAND, PlotTypes.PLOT_OCEAN;
+	Mutate(this, from, to, nil, expand);
+end
+
 
 function ANC_LandAndSea(this)
 
 
-	for i=1,4 do -- 7 attempts (random number)
-		local from, to = PlotTypes.PLOT_OCEAN, PlotTypes.PLOT_LAND;
-		local chance = 800;
-		--if (i%2==0) then local temp = from; from = to; to = temp; chance = 1000 - chance; end
-		Mutate(this, from, to);
+	for i=1,8 do
+		growLand(this);
+		fillLand(this);
+	end
+
+	for i=1,2 do
+		expandWater(this);
 	end
 	--[[
 	local haltonPointsX = halton(2, 600, 2 + Map.Rand(6,"Halton Rng"));
